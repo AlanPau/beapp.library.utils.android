8d31d7425d61515264004d6bba6118952b4c6cb5
AlanPau <a.paugois@beapp.fr>
2024-07-01 16:41:54 +0200

    build(): fusion between old and new version

--- utils/src/main/java/fr/beapp/utils/android/graphics/SelectorPressedType.java
+++
@@ -1,5 +0,0 @@
-package fr.beapp.utils.android.graphics;
-
-public enum SelectorPressedType {
-	BRIGHTER, DARKER
-}
--- utils/src/main/java/fr/beapp/utils/enums/EnumFromWS.java
+++
@@ -1,7 +0,0 @@
-package fr.beapp.utils.enums;
-
-public interface EnumFromWS {
-
-	String getKey();
-
-}
